643289419e65845ee9345428a7adabde4fc43a5e
Dubernet <thoms.dubernet@gmail.com>
2020-08-12 10:46:00 +0200

    structure devis

--- index3.php
+++ index3.php
@@ -1,9 +1,9 @@
 <?php
-// extract(unserialize(file_get_contents('datas.txt')));
+
 $conf = [
     'id' => '1',
     'facture_id' => '1',
-    'word' => 'dev',
+    'work' => 'dev',
     'dir' => null,
     'siret' => '879 724 342 00016',
     'adresse' => '25 rue aliénor d\'aquitaine',
@@ -42,15 +42,33 @@ $details = [
         'price' => '00'
     ]
 ];
+$date = new DateTime();
+$dateValid = new DateTime(); 
 ob_start();
 ?>
 <style>
+    *{
+        font-size: 7pt;
+    }
+    .t-6{
+        font-size: 6pt;
+    }
+    .t-11{
+        font-size: 11pt;
+    }
+    .t-15{
+        font-size: 15pt;
+    }
     table{
         width: 100%;
         vertical-align: top;
         border-collapse: collapse;
     }
-    table.border td {
+    td ul.list-work{
+        list-style-type: "- ";
+        padding-inline-start: 0;
+    }
+    /* table.border td {
         border:1px solid #CFD1D2;
         padding: 3mm 1mm;;
     }
@@ -72,16 +90,16 @@ ob_start();
     }
     tr.black{
         vertical-align: middle;
-    }
+    } */
     hr{
         height: 1px;
         background: #000000;
         border: none;
     }
-
+/* 
     table.paiement p{
         margin: 0;
-    }
+    } */
 
 </style>
 
@@ -99,7 +117,7 @@ ob_start();
                     <table>
                         <tr>
                             <td style="width:60%;">Date de validité du devis :</td>
-                            <td style="width:20%;">31 août 2020</td>
+                            <td style="width:20%;"><?php $dateValid->modify('+1 month'); echo $dateValid->format('d/m/y') ?></td>
                             <td style="width:20%;"></td>
                         </tr>
 
@@ -112,7 +130,7 @@ ob_start();
                         <tr>
                             <td style="width:60%;"><strong>Total hors taxe:</strong></td>
                             <td style="width:20%;"></td>
-                            <td style="width:20%;">300, 00€</td>
+                            <td style="width:20%; text-align:right;">300, 00€</td>
                         </tr>
                         <tr>
                             <td style="width:60%;">TVA non applicable - article 293 B du CGI</td>
@@ -125,10 +143,10 @@ ob_start();
                     
                     <table>
                         <tr>
-                            <td style="width:60%;">
+                            <td class="t-6" style="width:60%;">
                             <em>Conditions de règlement à 30 jours date de facture. Aucun escompte en cas de paiement anticipé.
-                                    Possibilité de paiement en plusieurs fois établit avec le client avant signature de ce contrat.</em> <br>
-                                    <strong>Mise en production direectement à compter de la réception du devis dûment signé.
+                                    Possibilité de paiement en plusieurs fois établit avec le client avant signature de ce contrat.</em> <br><br>
+                                    <strong>Mise en production directement à compter de la réception du devis dûment signé.
                                             A la validation du devis, un acompte de 30% vous sera demandé.</strong>
                             </td>
                             <td style="width:40%;"></td>
@@ -143,7 +161,7 @@ ob_start();
                                 <strong>BIC : CMCIFRP1MON - IBAN : FR76 1469 0000 0154 0000 3385 844</strong>
                             </td>
                             <td style="width:20%;">Bon pour accord le :</td>
-                            <td style="width:20%;"><strong>Signature</strong></td>
+                            <td style="width:20%; text-align:right;"><strong>Signature</strong></td>
                         </tr>
                     </table>
                 </td>
@@ -154,23 +172,52 @@ ob_start();
     <page_header backtop="25mm">
         <table>
             <tr>
-                <td class="moi">
-                    <strong><?= $conf['name'] ?></strong><br>
-                    <?= nl2br($conf['adresse']) ?><br>
-                    <strong>SIRET: <?= $conf['siret'] ?></strong><br>
-                    <em>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, deleniti minus. Nemo corporis quisquam molestiae ab accusamus illo magnam.</em>
+                <td style="width: 40%; text-align:center;">
+                    <strong class="t-15">Thomas Dubernet</strong>
                 </td>
-                <td class="client">
-                    <strong><?= $c['name'] ?></strong><br>
-                    <?= nl2br($c['infos']) ?><br>
-                    <strong>SIRET: <?= $c['siret'] ?></strong>
+                <td class="t-11" style="width: 30%">
+                    Développeur web & mobile.<br>
+                    -<br>
+                    Contact<br>
+                    +33 (0)6 11 90 59 49<br>
+                    contact@t4code.fr
+                </td>
+                <td class="t-11" style="width: 30%">
+                    Siret : 879 724 342 00016<br>
+                    -<br>
+                    Networks<br>
+                    t4code.fr<br>
+                    linkedin.com/in/thomas-dubernet
                 </td>
             </tr>
         </table>
-        <table style="margin-top:10mm;">
+        <br><br>
+        <table>
             <tr>
-                <td style="width:50%; text-align:left;font-size:120%;"><strong>Devis n°<?= str_replace('/', '', date('d/m/y')) ?> </strong></td>
-                <td style="width:50%; text-align:right;">Emis le <?= date('d/m/y') ?></td>
+                <td style="width: 20%;">
+                    <strong style="font-size: 12pt;">devis client |</strong>
+                </td>
+                <td style="font-size: 8pt;width: 20%;">
+                    <strong style="font-size: 8pt;">
+                    Stéphane martin<br>
+                    SOLutions+
+                    </strong><br>
+                    -<br>
+                    Contact<br>
+                    +33 (0)6 11 90 59 49<br>
+                    contact@t4code.fr
+                </td>
+                <td style="font-size: 8pt;width: 20%">
+                    235 Allée Isaac Newton<br>
+                    33127 Saint Jean D'Illac
+                </td>
+                <td style="font-size: 12pt;width: 20%; text-align: right;">
+                    <strong class="t-11">Devis n°<?= str_replace('/', '', date('d/m/y')) ?> </strong>
+                </td>
+                <td style="font-size: 8pt;width: 20%; text-align: right;">
+                    Martillac<br>
+                    le <?= $date->format('d/m/y') ?>
+                </td>
             </tr>
         </table>
 
@@ -201,8 +248,31 @@ ob_start();
                     </tr>
                 </tbody>
             </table>
-        <?php break; case "developper": ?>
+        <?php break; case "dev": ?>
+            <table style="width:100%; margin-top: 10mm;">
+                <tr>
+                    <td style="width: 20%;">
+                        Nature de la prestation |
+                    </td>
 
+                    <td style="width:60%;">
+                        <strong>-<br>
+                        Web Design | Maquetage</strong>
+                        <ul class="list-work">
+                            <li>Choix typo</li>
+                            <li>Choix colorimétrique</li>
+                            <li>Choix images</li>
+                            <li>Web design de chaque pages</li>
+                        </ul>
+                    </td>
+
+                    <td style="width:20%;text-align: right;">
+                    <br>
+                        300, 00€<br>
+                        1 jr
+                    </td>
+                </tr>
+            </table>
         <?php break; ?>
         <?php endswitch; ?>
     </page_header>
